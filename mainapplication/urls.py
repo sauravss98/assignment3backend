313192321e85101b2427fe495d84ff43a3b7ca7b
Inferno0990 <sauravsuresh171@gmail.com>
2022-07-25 12:37:30 +0530

    minor path changes

--- mainapplication/urls.py
+++ mainapplication/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns =[
-    path('',views.UserList.as_view()),
-    path('<int:pk>/',views.UserDetails.as_view()),
+    path('user/',views.UserList.as_view()),
+    path('user/<int:pk>/',views.UserDetails.as_view()),
+
 ]
